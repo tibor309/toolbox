18a3b0c579b8a9a0bfd0c24a4b2e1a2b22c451c3
Tibor Mercs <tibormercs8@gmail.com>
2023-10-03 12:06:00 +0200

    Added avatar command

--- commands/members.py
+++ commands/members.py
@@ -10,16 +10,35 @@ class members(commands.Cog):
     member = discord.SlashCommandGroup("member", hidden=False)
     
     @member.command()
-    async def avatar(self, ctx):
-      await ctx.respond(f"avatar")
-    
+    @discord.option("member", discord.Member, description="Select a user", required=True)
+    async def avatar(self, ctx: commands.Context, member: discord.Member) -> None:
+        png = member.avatar.with_format("png")
+        jpg = member.avatar.with_format("jpg")
+        webp = member.avatar.with_format("webp")
+
+        try:
+            gif = member.avatar.with_format("gif")
+        except:
+            gif = None
+
+        embed = discord.Embed(
+            description=f"[PNG]({png}) " f"[JPG]({jpg}) " f"[WEBP]({webp})" f" [GIF]({gif})"
+            if gif
+            else f"[PNG]({png}) -" f" [JPG]({jpg}) -" f" [WEBP]({webp})",
+            color=bot_color)
+
+        embed.set_author(name=f"{member.display_name}'s avatar", icon_url=image_icon)
+        embed.set_image(url=member.avatar)
+        await ctx.respond(embed=embed)
+
+
     @member.command()
     async def guild_avatar(self, ctx):
-      await ctx.respond(f"guild_avatar")
+        await ctx.respond(f"guild_avatar")
 
     @member.command()
     async def banner(self, ctx):
-      await ctx.respond(f"banner")
+        await ctx.respond(f"banner")
 
 
 
