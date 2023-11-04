2b912e2f632df2491c3a5516edc143a542042051
Tibor Mercs <tibormercs8@gmail.com>
2023-11-04 19:28:03 +0100

    Added member info command to context menu

---
+++ context_menus/context_member.py
@@ -0,0 +1,44 @@
+import discord
+from discord.ext import commands
+from config import bot_color, member_icon
+
+class context_member(commands.Cog):
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot = bot
+
+
+
+    @discord.user_command(name="Show info")
+    @discord.commands.default_permissions(kick_members=True)
+    async def info_member(self, ctx: commands.Context, member: discord.Member) -> None:
+        creation_time = int(member.created_at.timestamp())
+        join_time = int(member.joined_at.timestamp())
+    
+        embed = discord.Embed(color=bot_color)
+        embed.set_thumbnail(url=member.avatar)
+        embed.set_author(name="Member info", icon_url=member_icon)
+        embed.add_field(name="User mention", value=member.mention, inline=True)
+        embed.add_field(name="User name", value=member.name, inline=True)
+        embed.add_field(name="Nickname", value=member.nick, inline=True)
+            
+        embed.add_field(name="Server booster", value=bool(member.premium_since), inline=True)
+        embed.add_field(name="In voice", value=bool(member.voice), inline=True)
+        embed.add_field(name="Timed out", value=bool(member.timed_out), inline=True)
+        
+        if member.activity != None:
+            embed.add_field(name="Activity", value=member.activity, inline=False)
+        else:
+            embed.add_field(name="Activity", value="*None/hidden or offline*", inline=False)
+
+        embed.add_field(name="Bot", value=member.bot, inline=True)
+        embed.add_field(name="Status", value=member.status, inline=True)
+        embed.add_field(name="User ID", value=f"||{member.id}||", inline=True)
+        
+        embed.add_field(name="Account created", value=f"<t:{creation_time}:R>", inline=True)
+        embed.add_field(name="Joined server", value=f"<t:{join_time}:R>", inline=True)
+        await ctx.respond(embed=embed, ephemeral=True)
+
+
+
+def setup(bot: commands.Bot) -> None:
+      bot.add_cog(context_member(bot))
